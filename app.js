b8d1d2c805499aada7a33a897497f6adb37360bc
Ayan1009 <ayan10092018@gmail.com>
2024-07-26 12:36:12 +0530

    Second commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Part - I
+
 // const container = document.getElementById('react-container')
 
 // ReactDOM.render("Hello, Welcome to React", container)
@@ -12,22 +14,41 @@
 // const container = document.getElementById('react-container');
 // ReactDOM.render(React.createElement(Container),container);
 
-class ReactContainer extends React.Component{
-    // constructor - to initialize the state 
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //       name: 'Ayan' // Initialize the state with a name
-    //     };
-    // }
+// class ReactContainer extends React.Component{
+//     // constructor - to initialize the state 
+//     // constructor() {
+//     //     super();
+//     //     this.state = {
+//     //       name: 'Ayan' // Initialize the state with a name
+//     //     };
+//     // }
 
-    // render method to render the react dom 
-      render(){
-          return React.createElement(`div`,null,`Hey Kalvian`,
-          React.createElement(`div`,null,`Let's rock and roll with classes`)
-          );
-      }
+//     // render method to render the react dom 
+//       render(){
+//           return React.createElement(`div`,null,`Hey Kalvian`,
+//           React.createElement(`div`,null,`Let's rock and roll with classes`)
+//           );
+//       }
     
+//   }
+//   const container = document.getElementById('react-container');
+//   ReactDOM.render(React.createElement(ReactContainer),container);
+
+
+//Part - II
+
+class ReactContainer extends React.Component{
+    // JSX tags
+    // JSX there should be one parent element
+      render(){
+          return (
+          <div>
+            Hello! Welcome to Kalvium   
+             <div>This is babel</div>
+         </div> )
+      };  
   }
-  const container = document.getElementById('react-container');
-  ReactDOM.render(React.createElement(ReactContainer),container);
+
+const container = document.getElementById('react-container');
+// Direclty pass the component to render
+ReactDOM.render(<ReactContainer/>,container);    
